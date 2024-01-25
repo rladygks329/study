ff50c247e1e444f544a752f0d73472d8c5f3a587
Kim Yo Han <rladygks329@gmail.com>
2024-01-25 14:23:28 +0900

    feat: memberService
    1.transactional은 spring에서 제공하는 annotation을 통해 더 많은 옵션을 제어 가능하다.
    2. 데이터를 변경하지 않는 경우 readOnly  = true를 붙여서 읽기 성능을 향상 시킬 수 있다.
    3. 중복된 이름을 체크하는 비즈니스 로직 같은 경우에는 DB에서 unique 제약조건을 통해서 동시성 문제를 해결할 수 있다.
    4. auto wired를 사용하지 않는 이유는, 변경이 불가능하여 테스트 하기 어려워지기 때문이고,
    setMethod와 같은 경우는 런타임 환경에서 바뀔 수 있는 가능성이 생기므로 추천되지 않는다.
    따라서 생성자 주입을 권장하는 편이다.
    
    - spring 최근 버전은 생성자에 @autowired가 없어도 자동으로 주입해준다.
    - 주입받은repository는 변경 불가능하게 Final로 선언하자
    -> 해당 코드를 직접 작성할 수 있지만 lombok의 @RequiredConstrucotr를 활용하면, 기본적으로 notNull이나 private final을 가진 필드들을 가진 생성자를 static 메서드로 만들어준다.

---
+++ boot-jpa/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /*
+    * 회원 가입
+    * */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){
+        //EXCEPTION
+        List<Member> findMember = memberRepository.findByName(member.getName());
+        if(!findMember.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /*
+    * 전체 회원 조회
+    */
+    public List<Member> findAll(){
+        return  memberRepository.findAll();
+    }
+
+    public Member findById(Long id){
+        return  memberRepository.findOne(id);
+    }
+}
